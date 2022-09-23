d4a216755c28624f492f68318f3eb5f7d90308e8
Ohjinseo <dhwlstj@gmail.com>
2022-09-23 19:39:47 +0900

    메모추가

--- src/components/state.js
+++ src/components/state.js
@@ -165,7 +165,7 @@ const State = () => {
     const removeState = state.filter((item) => item.id !== e.target.value);
     //filter 는 제거하라는 명령문이 아니라
     //해당 상태가 맞지 않는 데이터를 제외하고 읽어오는 것이므로
-    //원본 데이터를 훼손한 상태가 아니다.
+    //원본 데이터를 훼손한 상태가 아니다.dsa
     setState(removeState);
   };
 
